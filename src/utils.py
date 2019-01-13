f8eda3f904a5297c03e76050688d0576a0ca1199
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2019-01-13 01:04:05 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -51,9 +51,11 @@ def feature_calculator(args, graph):
     return target_matrices
 
 def adjacency_opposite_calculator(graph):
+    """
+    Creating no edge indicator matrix.
+    :param graph: NetworkX object.
+    :return adjacency_matrix_opposite: Indicator matrix.
+    """
     adjacency_matrix = sparse.csr_matrix(nx.adjacency_matrix(graph),dtype=np.float32).todense()
     adjacency_matrix_opposite = np.ones(adjacency_matrix.shape)-adjacency_matrix
     return adjacency_matrix_opposite
-    
-
-    
